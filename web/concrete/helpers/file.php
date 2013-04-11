8992fb3f78243ec238719505c2f16f9aa23e10c6
hissy <hishikawa@concrete5.co.jp>
2013-04-12 04:07:12 +0900

    #124 ファイル名のサニタイズで拡張子だけになった時はmd5を通してファイル名を作る

--- web/concrete/helpers/file.php
+++ web/concrete/helpers/file.php
@@ -21,4 +21,21 @@
  */
 
 defined('C5_EXECUTE') or die("Access Denied.");
-class FileHelper extends Concrete5_Helper_File {}
+class FileHelper extends Concrete5_Helper_File {
+	
+	/** 
+	 * Cleans up a filename and returns the cleaned up version
+	 * @param string $file
+	 * @return string @file
+	 */
+	public function sanitize($file) {
+		// $file = preg_replace("/[^0-9A-Z_a-z-.\s]/","", $file); // pre 5.4.1 allowed spaces
+		$pathinfo = pathinfo($file);
+		$extension = !empty($pathinfo['extension']) ? '.' . $pathinfo['extension'] : '';
+		$file = str_replace($extension, '', $file);
+		$file_sanitized = preg_replace(array("/[\s]/","/[^0-9A-Z_a-z-.]/"),array("_",""), $file);
+		$file = (strlen($file_sanitized) > 0) ? $file_sanitized . $extension : md5($file) . $extension;
+		return trim($file);
+	}
+
+}
